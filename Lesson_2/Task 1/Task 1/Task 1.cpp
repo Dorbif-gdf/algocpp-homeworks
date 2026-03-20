d34a23d79d088f58c5a7d7570cd8e82fd4086c1b
Neto test <kitropllay@gmail.com>
2026-03-20 20:58:31 +0300

    Lesson_2

---
+++ Lesson_2/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int result = fib(n);
+
+    cout << "Fibonacci number: " << result << endl;
+
+    cout << "Time complexity: O(2^n)" << endl;
+    cout << "Memory complexity: O(n)" << endl;
+
+}
---
+++ Lesson_2/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+
+    int* f = new int[n + 1];
+
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+
+    int result = f[n];
+
+    delete[] f;
+
+    return result;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int result = fib(n);
+
+    cout << "Fibonacci number: " << result << endl;
+    cout << "Time complexity: O(n)" << endl;
+    cout << "Memory complexity: O(n)" << endl;
+
+}
